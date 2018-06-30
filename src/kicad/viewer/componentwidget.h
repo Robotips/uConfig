78e1fb4f45973158200391dbe7b8872f1d2b419e
sebcaux <sebastien.caux@robotips.fr>
2018-07-01 00:08:12 +0200

    Fixed nullptr for old gcc

--- src/kicad/viewer/componentwidget.h
+++ src/kicad/viewer/componentwidget.h
@@ -13,7 +13,7 @@ class KICAD_EXPORT ComponentWidget : public QWidget
 {
     Q_OBJECT
 public:
-    explicit ComponentWidget(QWidget *parent = nullptr);
+    explicit ComponentWidget(QWidget *parent = Q_NULLPTR);
 
     Component *component() const;
     void setComponent(Component *component);
